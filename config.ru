af97ea45a6d1a62733f61cd55717d4b93256f2ca
Tung Nguyen <tongueroo@gmail.com>
2012-05-31 09:50:40 -0700

    Change hello app to simple rack app

--- config.ru
+++ config.ru
@@ -1,4 +1,1 @@
-# This file is used by Rack-based servers to start the application.
-
-require ::File.expand_path('../config/environment',  __FILE__)
-run Hello::Application
+run Proc.new {|env| [200, {"Content-Type" => "text/html"}, ["OK"]]}
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hello::Application.config.secret_token = '924c6584a0f8bf49c3565587f96efbdb389503f56a56e5825eb6bc1a4dddf2ef2d6d4946004ce143969556d0ac5aa1b3636688cccd25ffad63592048806539f9'
