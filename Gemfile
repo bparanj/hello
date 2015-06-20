29612307741d566f685b30d1f2de8290d795b14d
Bala Paranj <bparanj@gmail.com>
2015-06-20 05:39:44 -0700

    Fix broken tests

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rack'
-gem 'rack-test'
 gem 'sinatra'
+
+group :test do
+  gem 'rack-test'
+end
--- hello_test.rb
+++ hello_test.rb
@@ -14,7 +14,7 @@ class HelloWorldTest < Test::Unit::TestCase
   def test_it_says_hello_world
     get '/'
     assert last_response.ok?
-    assert_equal 'Hello World', last_response.body
+    assert_equal 'Hello World : 2.2.2', last_response.body
   end
 
 end
